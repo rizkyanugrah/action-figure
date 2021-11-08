a5edb917cb3df020d8b0728c5e69036d1c4140a1
MRizkyAnugrah <muhammadrizkyanugrah01@gmail.com>
2021-11-08 22:28:44 +0800

    login

--- script.py
+++ script.py
@@ -4,15 +4,15 @@ import pandas as pd
 from tabulate import tabulate
 
 
-class Application:
+class Application:  
     CSV_FILE = 'data.csv'
-    USER_FILE = 'akun.csv'
     
-    # users = {
-    #     "username" :  ["rizky", "admin"],
-    #     "password" : ["123", "admin"],
-    #     "ID" : [1,2]
-    # }
+    users = {
+        "role"     : ['user','admin'],
+        "username" :  ["rizky", "admin"],
+        "password" : ["123", "admin"],
+        "ID" : [1,2]
+    }
 
     def cls(self):
         os.system('cls||clear')
@@ -20,41 +20,30 @@ class Application:
         return
 
     def register(self, name, password):
-        file = open(self.USER_FILE, 'a')
-        file.write(f"{name},{password}")
+        print('register')
 
-    def access(self, option):
-        if (option == "login"):
-            username = input("masukan username anda : ")
-            password = input("masukan password anda : ")
-            self.login(username, password)
-        else:
-            print("masukan username yang ingin anda tambahkan! ")
-            username = input("masukan username : ")
-            password = input('masukan password : ')
-            self.register(username, password)
-            print("sukses, silahakan login >_<")
-            self.access("login")
+    # def access(self, option):
+    #     if (option == "login"):
+    #         username = input("masukan username anda : ")
+    #         password = input("masukan password anda : ")
+    #         self.login(username, password)
+    #     else:
+    #         print("masukan username yang ingin anda tambahkan! ")
+    #         username = input("masukan username : ")
+    #         password = input('masukan password : ')
+    #         self.register(username, password)
+    #         print("sukses, silahakan login >_<")
+    #         self.access("login")
 
     def login(self, name, password):
-        login = False
-        file = open(self.USER_FILE, "r")
-        for i in file:
-            print()
-            a, b = i.split(",")
-            b = b.strip()
-            if(a == name and b == password):
-                login = True
-                break
-        file.close()
-        if(login == True):
-            print("sukses, silahkan masuk")
-        # search_user = self.users.get("username").index(name)
-        # print (search_user)
-        # if self.login in self.users.get("username")[search_user] and Password == self.users.get("password")[search_user]:
-        #     print("Berhasil Login")
-        # else:
-        #     print("Gagal Login Password Anda Salah!")
+        search_user = self.users.get("username").index(name)
+        if self.users.get("username")[search_user] and password == self.users.get("password")[search_user]:
+            if self.users.get("role")[search_user] == "admin":
+                return self.menuListAdmin(),"admin"
+            else:
+                return self.menuList(), "user"
+        else:
+            print("Gagal Login Password Anda Salah!")
 
     def showAll(self):
         print('DAFTAR ACTION FIGURE')
@@ -112,6 +101,15 @@ class Application:
     def menuList(self):
         print('\nMenu')
         print('[1] Daftar Action Figure')
+        print('[99] Selesai')
+
+        menu = int(input('Masukan menu yang ingin dipilih : '))
+
+        return menu
+    
+    def menuListAdmin(self):
+        print('\nMenu')
+        print('[1] Daftar Action Figure')
         print('[2] Tambah Action Figure')
         print('[3] Ubah Action Figure')
         print('[4] Hapus Action Figure')
@@ -128,46 +126,82 @@ class Application:
         print("Silahkan register jika anda belum memiliki akun")
 
         option = input("(Login/register) : ")
-
-        if(option != "login" and option != "register"):
-            self.start()
+                
+        if (option == "login"):
+            username = input("masukan username anda : ")
+            password = input("masukan password anda : ")
+            abc,role = self.login(username, password)
         else:
-            self.access(option)
+            print("masukan username yang ingin anda tambahkan! ")
+            username = input("masukan username : ")
+            password = input('masukan password : ')
+            self.register(username, password)
+            print("sukses, silahakan login >_<")
+            self.access("login")
 
         self.cls()
         terminate = False
 
-        # self.showAll()
+        self.showAll()
 
         try:
             while terminate == False:
-                menu = self.menuList()
+                # menu = self.menuList()
+                if role == "admin":
+                    menu = self.menuListAdmin()
+                    if menu == 1:
+                        self.cls()
+                        self.showAll()
+                    elif menu == 2:
+                        self.cls()
+                        self.showAll()
 
-                if menu == 1:
-                    self.cls()
-                    self.showAll()
-                elif menu == 2:
-                    self.cls()
-                    self.showAll()
+                        self.insertData()
+                        self.cls()
+                        self.showAll()
+                    elif menu == 3:
+                        self.cls()
+                        self.changeData()
 
-                    self.insertData()
-                    self.cls()
-                    self.showAll()
-                elif menu == 3:
-                    self.cls()
-                    self.changeData()
+                        self.cls()
+                        self.showAll()
+                    elif menu == 4:
+                        self.cls()
+                        self.deleteData()
 
-                    self.cls()
-                    self.showAll()
-                elif menu == 4:
-                    self.cls()
-                    self.deleteData()
-
-                    self.cls()
-                    self.showAll()
-                elif menu == 99:
-                    terminate = True
+                        self.cls()
+                        self.showAll()
                 else:
+                    menu = self.menuList()
+                    if menu == 1:
+                        self.cls()
+                        self.showAll()
+                
+                # if menu == 1:
+                #     self.cls()
+                #     self.showAll()
+                # elif menu == 2:
+                #     self.cls()
+                #     self.showAll()
+
+                #     self.insertData()
+                #     self.cls()
+                #     self.showAll()
+                # elif menu == 3:
+                #     self.cls()
+                #     self.changeData()
+
+                #     self.cls()
+                #     self.showAll()
+                # elif menu == 4:
+                #     self.cls()
+                #     self.deleteData()
+
+                #     self.cls()
+                #     self.showAll()
+                # elif menu == 99:
+                #     terminate = True
+                # else:
                     print('Masukan pilihan yang tersedia')
         except KeyboardInterrupt:
             print("\nSampai nanti!")
